629ad687300be342016a7a607bfcb94eefb45db3
sapphyree <sonexix@gmail.com>
2021-09-05 18:20:55 +0100

    Add event handling

--- src/index.js
+++ src/index.js
@@ -4,10 +4,21 @@ const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS] });
 
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	}
+	else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
@@ -16,26 +27,9 @@ for (const file of commandFiles) {
 	client.commands.set(command.data.name, command);
 }
 
-// When client is ready, run this code once
-client.once('ready', () => {
-	console.log('Ready!');
-});
-
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
-
-	const command = client.commands.get(interaction.commandName);
-
-	if (!command) return;
-
-	try {
-		await command.execute(interaction);
-	}
-	catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
-});
-
 // Login to Discord with client's token
 client.login(token);
+
+client.on('guildMemberAdd', () => {
+	console.log('News role assigned.');
+});
