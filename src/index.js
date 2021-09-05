76d42f460ff33b70d659876242edae5f7e8d72a7
sapphyree <sonexix@gmail.com>
2021-09-05 16:33:12 +0100

    Replace if / else if commands with dynamic handler

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 // Require base discord.js classes
-const { Client, Intents } = require('discord.js');
+const { fs } = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./src/commands/${file}`);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
 
 // When client is ready, run this code once
 client.once('ready', () => {
@@ -13,19 +24,16 @@ client.once('ready', () => {
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
-	const { commandName } = interaction;
+	const command = client.commands.get(interaction.commandName);
 
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
 	}
-	else if (commandName === 'server') {
-		await interaction.reply('Server info.');
-	}
-	else if (commandName === 'user') {
-		await interaction.reply('User info.');
-	}
-	else if (commandName === 'makemember') {
-		await interaction.reply('Made you a Member!');
+	catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
 });
 
